4106578050793bcedf5be3b321f5f853dfe5e1b5
Vashka-code <ivantrushkof@gmail.com>
2024-07-05 20:05:14 +0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Title from "../components/Title/Title";
 
 const Home = () => {
-  return <Title></Title>;
+  return <Title text="Home" />;
 };
 
 export default Home;
