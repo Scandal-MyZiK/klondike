6b6c33a6e9d576853449c6e06bd1a9c91f663664
Your Name <yang.lin.99@bk.ru>
2024-07-06 10:55:19 +0300

    HistoryItem

--- src/components/HistoryItem/HistoryItem.jsx
+++ src/components/HistoryItem/HistoryItem.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import "./style.css";
+import HistoryItemImageTelegram from "./assets/Telegram-img.svg";
+import HistoryItemImageArrow from "./assets/arrow-img.svg";
 
-export const HistoryItem = () => {
-  return <div>HistoryItem</div>;
+export const HistoryItem = ({ text, price }) => {
+  return (
+    <div className="historyItem">
+      <img
+        className="historyItem__img-telegram"
+        src={HistoryItemImageTelegram}
+        alt=""
+      />
+      <div className="historyItem__block">
+        <span className="historyItem__text">{text}</span>
+        <span className="historyItem__price">{price}</span>
+      </div>
+      <button className="historyItem__btn">
+        <span className="historyItem__desc">Start</span>
+        <img
+          className="historyItem__img-arrow"
+          src={HistoryItemImageArrow}
+          alt=""
+        />
+      </button>
+    </div>
+  );
 };
--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AppWidthWithContainer from "../../components/AppWidthContainer/AppWidthWithContainer";
 import Title from "../../components/Title/Title";
 import { Button } from "../../components/Buttons/Button";
+import { HistoryItem } from "../../components/HistoryItem/HistoryItem";
 
 import "./style.css";
 
@@ -10,6 +11,10 @@ const Tasks = () => {
     <AppWidthWithContainer>
       <Title text="TASKS" />
       <Button text="Active" />
+      <HistoryItem
+        text="Subscribe to our CEO channel (RU)"
+        price="+ $ 100,000"
+      />
     </AppWidthWithContainer>
   );
 };
