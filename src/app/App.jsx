1809f6042d2bfdfa8d9aa6331947f2cfdc195215
Vashka-code <ivantrushkof@gmail.com>
2024-07-08 18:27:24 +0300

    update loading

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,28 +1,43 @@
-import React, { Suspense, lazy } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "normalize.css";
 import Loading from "../components/Loading/Loading";
 import Navigation from "../components/Navigation/Navigation";
+import Home from "../pages/Home/Home";
+import Team from "../pages/Team/Team";
+import Tasks from "../pages/Tasks/Tasks";
+import RoadMap from "../pages/Roadmap/RoadMap";
 import "./assets/fonts/fonts.css";
 import "./app.css";
 
-const Home = lazy(() => import("../pages/Home/Home"));
-const Team = lazy(() => import("../pages/Tasks/Tasks"));
-const Tasks = lazy(() => import("../pages/Team/Team"));
-const RoadMap = lazy(() => import("../pages/Roadmap/RoadMap"));
-
 const App = () => {
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showLoading) {
+    return (
+      <div className="app-container">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <div className="app-container">
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/team" element={<Tasks />} />
-            <Route path="/tasks" element={<Team />} />
-            <Route path="/roadmap" element={<RoadMap />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Tasks />} />
+          <Route path="/tasks" element={<Team />} />
+          <Route path="/roadmap" element={<RoadMap />} />
+        </Routes>
         <Navigation />
       </div>
     </Router>
