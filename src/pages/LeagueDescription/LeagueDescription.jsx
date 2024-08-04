de2600170ef5e7ad9b6b291329080c528ac0929a
Vashka-code <ivantrushkof@gmail.com>
2024-08-04 23:47:23 +0300

    add background

--- src/pages/LeagueDescription/LeagueDescription.jsx
+++ src/pages/LeagueDescription/LeagueDescription.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppWidthWithContainer from "../../components/AppWidthContainer/AppWidthWithContainer";
 import "./style.css";
 import { LeagueSlider } from "../../components/LeagueSlider/LeagueSlider";
 
 const LeagueDescription = () => {
+  useEffect(() => {
+    // Add the class when the component mounts
+    const appContainer = document.querySelector(".app-container");
+    if (appContainer) {
+      appContainer.classList.add("app-container--league");
+    }
+
+    // Clean up the class when the component unmounts
+    return () => {
+      if (appContainer) {
+        appContainer.classList.remove("app-container--league");
+      }
+    };
+  }, []);
+
   return (
     <AppWidthWithContainer className="league-description">
       <div className="league-description__content">
